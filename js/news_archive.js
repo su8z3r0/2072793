0bec9cde141cf54a1a353ef40a8ef9893d0a30c5
su8z3r0 <ciroperna@libero.it>
2013-09-13 17:51:38 +0200

    Revert "New update"
    This reverts commit 840bd92011de3d2f46c7f1429ad4752ee0d0c171.

--- js/news_archive.js
+++ js/news_archive.js
@@ -4,108 +4,40 @@
     attach: function (context, settings) {
       
 	  var path = Drupal.settings.news_archive.news_archive_path;
-      
-	  //var block_type = Drupal.settings.news_archive.block_path;
-	  
-	  alert(block_type);
-	  
-	  $(".treeview").treeview({
-	  	collapsed: true,
-	  	animated: "fast",
-	  	prerendered: true,
-	  });
-	  
-	  /*
-	  $('.news-archive li').each(function(){
-	    if($(this).hasClass('active')){
-		  $(this).addClass('minusbox');
-		  $(this).children().css('display', 'block');
-		  if($(this).hasClass('news-archive-posts-leaf')){
-		    $(this).css('list-style', 'none');
-		  }
-		} else {
-		  $(this).addClass('plusbox');
-		  if($(this).hasClass('news-archive-posts-leaf')){
-		    $(this).css('list-style', 'none');
-		  }
-		}
-	  });*/
-	  
-	  
-	  /*
-	  $('.news-archive-wrapper li:has(ul)').click(function(event){
-		if (this == event.target) {
-		  $(this).toggleClass('active');
-		  alert("OK");
-		   //$('.news-archive li').each(function(){
-	      if($(this).hasClass('active')){
-		    if($(this).children().is(':hidden')){
-		      $(this).removeClass('plusbox');
-		      $(this).addClass('minusbox');
-		      $(this).children().css('display', 'block');
-		    } else {
-		      $(this).addClass('plusbox');
-	          $(this).removeClass('minusbox');
-		      if($(this).hasClass('news-archive-posts-leaf')){
-		        $(this).css('list-style', 'none');
-		      }
-		    }
-		    
-		    if($(this).hasClass('news-archive-posts-leaf')){
-		      $(this).css('list-style', 'none');
-		    }
-		  }
-		}
-	    //});
-	  });
-	  */
-	  /*
-	  $('.news-archive-wrapper li').css('pointer','default')
-      $('.news-archive-wrapper li')
-          .click(function(){
-              
-			  if( $(this).children('ul').is(':hidden')){
-                
-			  }
-			  /*
-			  if (this == event.target) {
-			    alert("OK");
-                $(this).toggleClass('active');
-                if(!$(this).children().is(':hidden')){
-                  $(this).addClass('plusbox');
-                  $(this).removeClass('minusbox');
-                } else {
-                  $(this).removeClass('plusbox');
-                  $(this).addClass('minusbox');
-                }				      
-                $(this).children().show();
-              }
-			  */
-       /*   })
-          .css({cursor:'pointer'})
-		  .addClass("plusbox")
-          
-      $('li:not(:has(ul))').css({cursor:'default', 'list-style-image':'none'});
 
-	  /*	  
+	  
+	  $('li')
+        .css('pointer','default')
+        .css('list-style-image','none');
+      $('li:has(ul)')
+          .click(function(event){
+              if (this == event.target) {
+                  $(this).css('list-style-image',
+                      (!$(this).children().is(':hidden')) ? 'url("' + path + '/images/plusbox.gif")' : 'url("' + path + '/images/minusbox.gif")').toggleClass('active');
+                  $(this).children().toggle('fast');
+              }
+              return false;
+          })
+          .css({cursor:'pointer', 'list-style-image':'url("' + path + '/images/plusbox.gif")'})
+          .children().hide();
+      $('li:not(:has(ul))').css({cursor:'default', 'list-style-image':'none'});
+	  
+	  $('li a').click(function(){
+	    //alert($(this).attr('href'));
+		document.location.href = $(this).attr('href');
+	  });
+
+	  	  
 	  $('.news-archive li').each(function(){
 	    if($(this).hasClass('active')){
 		  $(this).children('ul').toggle();
-          if($(this).children().is(':hidden')){
-            $(this).addClass('plusbox');
-            $(this).removeClass('minusbox');
-          } else {
-            $(this).removeClass('plusbox');
-            $(this).addClass('minusbox');
-          }
-		  if($(this).hasClass('news-archive-posts-leaf')){
-		    $(this).css('list-style', 'none');
-		  }
+		  $(this).css('list-style-image',
+                      (!$(this).hasClass('active')) ? 'url("' + path + '/images/plusbox.gif")' : 'url("' + path + '/images/minusbox.gif")');
 	    } else {
 		  $(this).removeClass('active');
 	    }
 	  });
-	  */
+	  
     }
   };
 
